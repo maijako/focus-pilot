d7c3c01440110b401b24f4d9f9a549a75961886d
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-01-30 11:56:25 +0000

    Adds starter file for background-image, adds code to get values from weather api

--- assets/js/weather-display.js
+++ assets/js/weather-display.js
@@ -1,18 +1,25 @@
-var latitude, longitude;
 // var APIKey = "e80fb51109675d7a9bc4ed534b97b3ae";
 var APIKey = "7346f2f976dfac403985b13d4b581099";
-var queryURL;
+var displayEl = $('#date-weather-display');
+
+// Accepts the temperature in Kelvin and returns the temperature in Celsius.
+var convertKelvinToCelsius = function(temp) {
+
+    var temInCelsius = temp - 273.15;
+    return temInCelsius.toFixed(0);
+
+};
 
 function success(userLocation) {
 
-    latitude = userLocation.coords.latitude;
-    longitude = userLocation.coords.longitude;
+    var latitude = userLocation.coords.latitude;
+    var longitude = userLocation.coords.longitude;
 
-    console.log(userLocation);
-    console.log("Latitude of current location: "+latitude);
-    console.log("Longitude of current location: "+longitude);
+    // console.log(userLocation);
+    // console.log("Latitude of current location: "+latitude);
+    // console.log("Longitude of current location: "+longitude);
 
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+APIKey;
 
     $.ajax({
 
@@ -22,6 +29,23 @@ function success(userLocation) {
     }).then(function(response) {
 
         console.log(response);
+        let currentTemp = convertKelvinToCelsius(response.main.temp);
+        let feelsLikeTemp = convertKelvinToCelsius(response.main.feels_like);
+        let locName = response.name;
+        let icon = response.weather[0].icon;
+        let weatherName = response.weather[0].main;
+        let weatherDescription = response.weather[0].description;
+
+        console.log("Current Temperature: "+currentTemp);
+        console.log("Fls like: "+feelsLikeTemp);
+        console.log("loc name: "+locName);
+        console.log("icon: "+icon);
+        console.log("weatherName: "+weatherName);
+        console.log("weatherDescription: "+weatherDescription);
+
+        // An image that contains the corresponding weather icon for the day.
+        // var imgIcon = $('<img>');
+        // imgIcon.attr('src', "https://openweathermap.org/img/wn/"+icon+"@2x.png");
 
     });
 
