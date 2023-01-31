722c469148747a7a2399fd4526a7a9db60d01ce1
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-31 23:26:19 +0200

    js added with a basic user breaktime activity suggestions liss

---
+++ assets/js/activitylist.js
@@ -0,0 +1,53 @@
+//A list of activities that the user is going to pick from during the break
+var fiveMinBreak = {
+  suggestion: "You have 5 minutes to do something different! Why not use them to ",
+  activities: [
+    "do some stretches",
+    "meditate",
+    "organize your desk or a drawer",
+    "do a five minute plank variation",
+    "do as many push-ups, squats or sit-ups as you can",
+    "have a glass of water"]
+};
+
+var tenMinBreak = {
+  suggestion: "You have 10 minutes to do something different! Why not use them to ",
+  activities: [
+    "do some stretches or yoga",
+    "tidy up your room",
+    "do a short exercise routine",
+    "make a snack and a beverage",
+    "make a shopping list"]
+};
+
+var fifteenMinBreak = {
+  suggestion: "You have 15 minutes to do something different! Why not use them to ",
+  activities: [
+    "follow a 15 min workout or meditation on YouTube",
+    "do some meal preparation for today or tomorrow",
+    "go outside for a brisk walk around the block",
+    "do a household task you've been putting off",
+    "write down a list of things you're grateful for"]
+};
+
+var twentyMinBreak = {
+  suggestion: "You've earned 20 minutes to do something different! Why not use them to ",
+  activities: [
+    "prepare a healthy meal or snack",
+    "go outside for a short walk",
+    "take a shower",
+    "read a book",
+    "do your favorite craft or a creative activity",
+    "practice something new"]
+};
+
+var twentyFiveMinBreak = {
+  suggestion: "You've earned 25 minutes to do something different! Why not use them to ",
+  activities: [
+    "go for a short walk or a jog",
+    "do a full-body workout session",
+    "clean up any mess or organize your living space",
+    "have a meal or a snack",
+    "read a novel",
+    "practice your favorite creative activity"]
+};
