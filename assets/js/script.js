b16f0f536434433d1bb17e364a0fa1cf565db32d
dodzikojo <dodzi@windowslive.com>
2023-02-03 08:39:07 +0000

    Create cards for all news from new API

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,26 +95,92 @@ $("#news-categories").children().on("click", function () {
 
 // GET NEWS API
 function getNewsAPI(topic) {
-  url = 'https://gnews.io/api/v4/top-headlines?token=' + GNewsAPIKey + '&topic=' + topic + '&lang=en&max=10';
+  // url = 'https://gnews.io/api/v4/top-headlines?token=' + GNewsAPIKey + '&topic=' + topic + '&lang=en&max=10';
 
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      articles = data.articles;
+  // fetch(url)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     articles = data.articles;
 
-      for (i = 0; i < articles.length; i++) {
-        // articles[i].title
-        console.log("Title: " + articles[i]['title']);
-        // articles[i].description
-        console.log("Description: " + articles[i]['description']);
-        // You can replace {property} below with any of the article properties returned by the API.
-        // articles[i].{property}
-        // console.log(articles[i]['{property}']);
+  //     for (i = 0; i < articles.length; i++) {
+  //       // articles[i].title
+  //       console.log("Title: " + articles[i]['title']);
+  //       // articles[i].description
+  //       console.log("Description: " + articles[i]['description']);
+  //       // You can replace {property} below with any of the article properties returned by the API.
+  //       // articles[i].{property}
+  //       // console.log(articles[i]['{property}']);
 
-        // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-        break;
-      }
-    });
+  //       // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
+  //       break;
+  //     }
+  //   });
+}
+
+createNewsCards("https://resize.indiatvnews.com/en/resize/newbucket/715_-/2020/09/breakingnews-live-blog-1568185450-1595123397-1601430958.jpg", "Alt Text", "News Title", "News Description", "category", "6:45pm")
+
+function createNewsCards(newsImageURL, newsImageALT, newsTitle, newsDescription, newsCategory, newsTime) {
+  let cardAnchor = $("<a>", {
+    class: "news-link col",
+    href: "#"
+  })
+
+  let mainCard = $("<div>", {
+    class: "card p-0"
+  })
+
+  let cardImg = $("<img>", {
+    class: "card-img-top",
+  })
+
+  let cardBody = $("<div>", {
+    class: "card-body news-card-body"
+  })
+
+  let cardTitle = $("<h5>", {
+    class: "card-title"
+  })
+
+  let cardText = $("<p>", {
+    class: "card-text news-card-text"
+  })
+
+  let cardCategoryBadge = $("<p>", {
+    class: "news-category badge rounded-pill bg-secondary py-1 my-1"
+  })
+
+  let cardFooter = $("<div>", {
+    class: "card-footer"
+  })
+
+  let cardFooterText = $("<p>", {
+    class: "card-text text-muted"
+  })
+
+  cardImg.attr("src", newsImageURL)
+  cardImg.attr("alt", newsImageALT)
+  cardTitle.text(newsTitle);
+  cardText.text(newsDescription);
+  cardCategoryBadge.text(newsCategory);
+  cardFooterText.text(newsTime);
+
+  cardImg.appendTo(mainCard)
+
+  cardTitle.appendTo(cardBody)
+  cardText.appendTo(cardBody)
+  cardCategoryBadge.appendTo(cardBody)
+
+  cardBody.appendTo(mainCard)
+
+  cardFooterText.appendTo(cardFooter)
+
+  cardFooter.appendTo(mainCard)
+
+  mainCard.appendTo(cardAnchor)
+
+
+  cardAnchor.appendTo($("#latest-news"))
+
 }
