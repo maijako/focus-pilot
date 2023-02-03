bcc8e33b2b30844a54828133910470a8de017d6a
dodzikojo <dodzi@windowslive.com>
2023-02-03 08:11:10 +0000

    Add click event for news category buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,28 +88,33 @@ function hideShowElements() {
   }
 }
 
+$("#news-categories").children().on("click", function () {
+  console.log($(this).attr("data-news-category"))
+})
+
 
 // GET NEWS API
-// let topic = "technology"
-// url = 'https://gnews.io/api/v4/top-headlines?token=' + GNewsAPIKey + '&topic=' + topic + '&lang=en&max=10';
+function getNewsAPI(topic) {
+  url = 'https://gnews.io/api/v4/top-headlines?token=' + GNewsAPIKey + '&topic=' + topic + '&lang=en&max=10';
 
-// fetch(url)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     articles = data.articles;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      articles = data.articles;
 
-//     for (i = 0; i < articles.length; i++) {
-//       // articles[i].title
-//       console.log("Title: " + articles[i]['title']);
-//       // articles[i].description
-//       console.log("Description: " + articles[i]['description']);
-//       // You can replace {property} below with any of the article properties returned by the API.
-//       // articles[i].{property}
-//       // console.log(articles[i]['{property}']);
+      for (i = 0; i < articles.length; i++) {
+        // articles[i].title
+        console.log("Title: " + articles[i]['title']);
+        // articles[i].description
+        console.log("Description: " + articles[i]['description']);
+        // You can replace {property} below with any of the article properties returned by the API.
+        // articles[i].{property}
+        // console.log(articles[i]['{property}']);
 
-//       // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-//       break;
-//     }
-//   });
+        // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
+        break;
+      }
+    });
+}
